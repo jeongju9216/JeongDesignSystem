df3b25bf8e03174385bc5b38bba58ce54ed4f14f
유정주 <dbwjdwn9216@gmail.com>
2024-04-16 20:31:02 +0900

    [#9] 코드로 Sample 앱의 ViewController 표시

--- JeongDesignSystem/Sample/Sample/ViewController.swift
+++ JeongDesignSystem/Sample/Sample/ViewController.swift
@@ -8,11 +8,62 @@
 import UIKit
 import JeongDesignSystem
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - UI
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.text = "JeongDesignSystem"
+        
+        return label
+    }()
+    
+    
+    // MARK: - Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Test.greeting()
+        
+        setUpAttributes()
+        setUpSubviews()
+        setUpConstraints()
+    }
+    
+    
+    // MARK: - Setup
+    
+    private func setUpAttributes() {
+        view.backgroundColor = .white
+    }
+    
+    private func setUpSubviews() {
+        view.addSubview(titleLabel)
+    }
+    
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Metric.TitleLabel.topOffset)
+        ])
     }
 }
+
+
+// MARK: - Constant
+
+private extension ViewController {
+    
+    enum Metric {
+        
+        enum TitleLabel {
+            
+            static let topOffset = 40.0
+        }
+    }
+}
+
