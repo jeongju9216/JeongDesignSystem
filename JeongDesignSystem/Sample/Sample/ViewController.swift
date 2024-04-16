e055aa0d28612082df87fd395798292c93668871
유정주 <dbwjdwn9216@gmail.com>
2024-04-16 18:23:26 +0900

    [#7] 샘플 앱 생성

---
+++ JeongDesignSystem/Sample/Sample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by 유정주 on 4/16/24.
+//
+
+import UIKit
+import JeongDesignSystem
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Test.greeting()
+    }
+}
