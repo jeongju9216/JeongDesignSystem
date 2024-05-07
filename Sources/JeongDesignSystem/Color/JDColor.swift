b47d6084a29b8ca011ea1a4926712b36fd805a15
유정주 <dbwjdwn9216@gmail.com>
2024-05-07 22:24:21 +0900

    [#16] JDColor에 gray 세트 추가

--- Sources/JeongDesignSystem/Color/JDColor.swift
+++ Sources/JeongDesignSystem/Color/JDColor.swift
@@ -11,4 +11,10 @@ public enum JDColor {
     
     static let primary = UIColor(hex: 0x00b6d7)
     static let background = UIColor.systemBackground
+    
+    // MARK: Gray
+    
+    static let lightGray = UIColor(hex: 0xE3E3E3)
+    static let gray = UIColor(hex: 0x999999)
+    static let darkGray = UIColor(hex: 0x404040)
 }
