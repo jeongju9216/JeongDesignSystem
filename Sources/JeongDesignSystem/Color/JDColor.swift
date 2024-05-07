512ad207fc02a2aa717a97d201e0c95aa1cd3fe1
유정주 <dbwjdwn9216@gmail.com>
2024-05-07 22:30:27 +0900

    [#16] JDColor의 각 상수를 public으로 변경

--- Sources/JeongDesignSystem/Color/JDColor.swift
+++ Sources/JeongDesignSystem/Color/JDColor.swift
@@ -9,12 +9,12 @@ import UIKit
 
 public enum JDColor {
     
-    static let primary = UIColor(hex: 0x00b6d7)
-    static let background = UIColor.systemBackground
+    public static let primary = UIColor(hex: 0x00b6d7)
+    public static let background = UIColor.systemBackground
     
     // MARK: Gray
     
-    static let lightGray = UIColor(hex: 0xE3E3E3)
-    static let gray = UIColor(hex: 0x999999)
-    static let darkGray = UIColor(hex: 0x404040)
+    public static let lightGray = UIColor(hex: 0xE3E3E3)
+    public static let gray = UIColor(hex: 0x999999)
+    public static let darkGray = UIColor(hex: 0x404040)
 }
